b40ac3b80d98224d9a6d6d4c5db5dc83030fff34
Jeremy Evans <jeremy.evans@mavs.uta.edu>
2022-07-21 18:08:11 -0500

    dependency

--- shoe-shop/src/index.js
+++ shoe-shop/src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createRoot } from "react-dom/client";
+// import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
